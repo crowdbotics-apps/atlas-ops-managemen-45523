5d01c5de47a2f6e8511b4bc4c5f3e85af07b0bd8
Crowdbotics <team@crowdbotics.com>
2024-01-24 21:07:21 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "atlas-ops-managemen-45523",
+  url: "https://atlas-ops-managemen-45523.botics.co",
+  api: "https://atlas-ops-managemen-45523.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
